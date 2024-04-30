3cefb9250f2f8be37b0ad66fe7cad18975f040ae
Daniel Vaz <danielvaz.tzk@hotmail.com>
2024-04-29 22:11:31 -0300

    Create at Controller and at a Models

---
+++ MoviesAPI.NET/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.NET.Models;
+
+namespace MoviesAPI.NET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void AddMovie([FromBody]Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+            Console.WriteLine(movie.Duration);
+        }
+
+    }
+}
---
+++ MoviesAPI.NET/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesAPI.NET.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
