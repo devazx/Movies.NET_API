22f9f7bc41dbe6c1ad1072250838009a639a63ed
Daniel Vaz <danielvaz.tzk@hotmail.com>
2024-04-29 22:40:50 -0300

    deployed data annotations in Movie Models

--- MoviesAPI.NET/Models/Movie.cs
+++ MoviesAPI.NET/Models/Movie.cs
@@ -1,9 +1,16 @@
-﻿namespace MoviesAPI.NET.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.NET.Models
 {
     public class Movie
     {
+        [Required(ErrorMessage = "The Title is required ")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Category if required")]
+        [MaxLength(50, ErrorMessage = "Category is not to exceed 50 character")]
         public string Category { get; set; }
+        [Required]
+        [Range(70,600, ErrorMessage = "A Duration must have between 70 and 600 minutes")]
         public int Duration { get; set; }
 
     }
